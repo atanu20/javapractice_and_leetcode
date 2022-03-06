587cf698d390b9638dc2a68fa208509cd928fcce
atanu <atanuj383@gmail.com>
2022-03-06 15:46:57 +0530

    Add

---
+++ array/Special_Array_With_X_Elements_Greater_Than_or_Equal_X.java
@@ -0,0 +1,19 @@
+package array;
+
+public class Special_Array_With_X_Elements_Greater_Than_or_Equal_X {
+    public int specialArray(int[] nums) {
+        int n = nums.length;
+        int ans = -1;
+        for (int i = 1; i < 1001; i++) {
+            int co = 0;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] >= i) {
+                    co++;
+                }
+            }
+            if (co == i)
+                ans = i;
+        }
+        return ans;
+    }
+}
